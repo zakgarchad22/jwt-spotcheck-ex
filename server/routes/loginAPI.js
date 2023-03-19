ec49521087655d418d4f7fa4b3f065fd0fc61a0a
ameerj95 <1995msj1995@gmail.com>
2023-03-19 11:33:54 +0200

    extra imports that shouldnt be there

--- server/routes/loginAPI.js
+++ server/routes/loginAPI.js
@@ -1,5 +1,3 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const users = require('../../fakeDB/users');
